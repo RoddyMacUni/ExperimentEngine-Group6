2e9af6359c51e6411bfcb07e52bbe8a0fc05fda0
Extremely-Original-Username <joshinnes05@gmail.com>
2025-05-14 12:20:33 +0100

    refactored counter

---
+++ src/processing/Counter.py
@@ -0,0 +1,28 @@
+from typing import Callable
+
+class Counter:
+    minorIncrement: int
+    majorIncrement: int
+
+    majorIncrementThreshold: int
+    onMajorIncrement: Callable[[int], None]
+
+    def __init__(self, majorIncrementThreshold: int, onMajorIncrement: Callable[[int], None]):
+        self.minorIncrement = 0
+        self.majorIncrement = 0
+
+        self.majorIncrementThreshold = majorIncrementThreshold
+        self.onMajorIncrement = onMajorIncrement
+
+    def increment(self):
+        self.minorIncrement += 1
+        if self.minorIncrement >= self.majorIncrementThreshold:
+            self.minorIncrement = 0
+            self.majorIncrement += 1
+            self.onMajorIncrement(self.majorIncrement)
+
+    def reset(self):
+        self.minorIncrement = 0
+        self.majorIncrement = 0
+            
+
