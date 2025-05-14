90b1550a83f739bf42208ab1599c373c5717e661
Extremely-Original-Username <joshinnes05@gmail.com>
2025-05-14 13:15:55 +0100

    Reapply "Added basic listener"
    This reverts commit 839d49f09f6600829f3e24a4cd275f6cc242897a.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,40 @@
 from api.ExperimentApi import ExperimentApi, Experiment
 from api.ResultApi import ResultApi, GenericResponse, ResultSet
 from AppSettings import AppSettings, GetAppSettings
-import json
+import os
+import time
+from processing.TimeCounter import TimeCounter
+
+EndTaskFlag: bool = False
 
 appSettings: AppSettings = GetAppSettings()
 
+def processExperiment(experimentId: str):
+    #Get data
+    experiment: Experiment = ExperimentApi(appSettings.ExperimentsEndpoint).getExperimentById("TODO")
+
+    #Run through network
+
+    #Get metrics
+
+    #Send result
+    resultResponse: GenericResponse = ResultApi(appSettings.ResultsEndpoint).sendResults("TODO", ResultSet("Test"))
+
+    print("Everything ran!")
+    print("ExperimentId: " + experiment.id)
+    print("ResultResponseMessage: " + resultResponse.message)
+
+print("Listening for file on " + appSettings.ListenerTargetFolder)
 #Listen for new file
+counter = TimeCounter(lambda minutes: print("No new file detected for " + str(minutes) + " minutes."))
+while not EndTaskFlag:
+    files = sorted(os.listdir(appSettings.ListenerTargetFolder), key=lambda filename: os.path.getmtime(appSettings.ListenerTargetFolder + "/" + filename), reverse=False)
+    files.remove("README.md")
 
-#Get data
-experiment: Experiment = ExperimentApi(appSettings.ExperimentsEndpoint).getExperimentById("TODO")
-
-#Run through network
-
-#Get metrics
-
-#Send result
-resultResponse: GenericResponse = ResultApi(appSettings.ResultsEndpoint).sendResults("TODO", ResultSet("Test"))
-
-print("Everything ran!")
-print("ExperimentId: " + experiment.id)
-print("ResultResponseMessage: " + resultResponse.message)
+    if len(files) > 0:
+        counter.reset()
+        print("File detected: " + files[0])
+        processExperiment(files[0])
+    else:
+        time.sleep(1)
+        counter.increment()
