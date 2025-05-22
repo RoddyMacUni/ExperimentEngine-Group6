e2d4c6c8ff2c67961d5cc79749c82d4d2a587ae7
JJInnes <joshinnes05@gmail.com>
2025-05-22 20:00:13 +0100

    updated data model

--- src/model/ResultSet.py
+++ src/model/ResultSet.py
@@ -1,6 +1,35 @@
 from dataclasses import dataclass
+from typing import List
 
-#TODO Implement this
+@dataclass 
+class Sequence:
+    ECT: str
+    COD: str
+    ECM: str
+    SPT: str
+    TMP: str
+    QUA: int
+    DPT: int
+    GAM: int
+    DURATION: int
+    VID: str
+
+@dataclass
+class VideoResultMetrics:
+    Subject: str
+    Bitrate: int
+    PSNR: int
+    SSIM: int
+    VMAP: int
+
+@dataclass
+class ResultSetItem:
+    Sequence: Sequence
+    Network: str
+    Results: List[VideoResultMetrics]
+
 @dataclass
 class ResultSet:
-    testParam: str
+    Target: str
+    Partner: str
+    Set: List[ResultSetItem]
