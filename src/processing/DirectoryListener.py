05918563426632b02006454ef61283b3f7707215
JJInnes <joshinnes05@gmail.com>
2025-05-27 21:28:40 +0100

    added poison folder for failures

--- src/processing/DirectoryListener.py
+++ src/processing/DirectoryListener.py
@@ -16,11 +16,18 @@ class DirectoryListener:
         self.ignoreFiles = ignoreFiles
         self.processorFunction = processorFunction
 
+        self.ignoreFiles.append("poison")
+
     #Start the listener, TODO this could probably be asyncronous
     #Can set a limit to to the number of loops for testing
     #Includes a counter to give some output while inactive
     def start(self, stopAfter: int = -1):
         print("Listening for file on " + self.targetFolder)
+
+        #Create folder for failed files
+        if not os.path.exists(self.targetFolder + "/poison"):
+            os.makedirs(self.targetFolder + "/poison")
+
         counter = Counter(60, lambda minutes: print("No new file detected for " + str(minutes) + " minutes."))
         
         while not self.stopFlag and (stopAfter < 0 or counter.total < stopAfter):
@@ -36,7 +43,8 @@ class DirectoryListener:
 
                 pattern = re.compile("[A-Za-z0-9-]+_encoded.mp4") #File-Id_encoded.mp4 would be valid, for example
                 if not pattern.fullmatch(files[0]):
-                    print("File name did not follow expected pattern")
+                    print("File name did not follow expected pattern, moving to poison queue")
+                    os.rename(self.targetFolder + "/" + files[0], self.targetFolder + "/poison/" + files[0])
                     continue
 
                 self.processorFunction(files[0].partition('.')[0].partition('_')[0], files[0]) #File id only, file full name
