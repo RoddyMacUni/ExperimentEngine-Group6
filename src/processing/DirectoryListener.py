05e6e6c3cb058619bb132a9fcf005a732ae3d596
JJInnes <joshinnes05@gmail.com>
2025-05-27 21:20:58 +0100

    added regex validation

--- src/processing/DirectoryListener.py
+++ src/processing/DirectoryListener.py
@@ -2,6 +2,7 @@ from processing.Counter import Counter
 import os
 import time
 from typing import Callable
+import re
 
 class DirectoryListener:
     targetFolder: str
@@ -24,7 +25,7 @@ class DirectoryListener:
         
         while not self.stopFlag and (stopAfter < 0 or counter.total < stopAfter):
             files = sorted(os.listdir(self.targetFolder), key=lambda filename: os.path.getmtime(self.targetFolder + "/" + filename), reverse=False)
-            
+
             #Ignore specified files
             for i in range(len(self.ignoreFiles)):
                 files.remove(self.ignoreFiles[i])
@@ -32,6 +33,11 @@ class DirectoryListener:
             if len(files) > 0:
                 counter.reset()
                 print("File detected: " + files[0])
+
+                pattern = re.compile("[A-Za-z0-9-]+_encoded.mp4") #File-Id_encoded.mp4 would be valid, for example
+                if not pattern.fullmatch(files[0]):
+                    print("File name did not follow expected pattern")
+                    continue
 
                 self.processorFunction(files[0].partition('.')[0].partition('_')[0], files[0]) #File id only, file full name
 
