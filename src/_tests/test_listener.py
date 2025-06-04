898a948bb82d35d216698be3f3cc85f6fe2c5eb2
JJInnes <joshinnes05@gmail.com>
2025-06-04 22:37:25 +0100

    added tests

--- src/_tests/test_listener.py
+++ src/_tests/test_listener.py
@@ -15,8 +15,14 @@ def processFiles(a: str, b: str, c: int):
     globals()["lastFileId"] = a
     globals()["lastFileName"] = b
 
+def processFilesWithUnexpectedError(a: str, b: str, c: int):
+    raise Exception("Unknown error")
+
+def processFilesWithKnownError(a: str, b: str, c: int):
+    raise KnownProcessingException("id", "Known error", 0, "partner")
+
 def knownErrorCallback(error: KnownProcessingException):
-    pass
+    globals()["knownErrorDetected"] = True
 
 def test_listener_can_process():
     listener = DirectoryListener(srcPath + "/in", ["README.md"], processFiles, knownErrorCallback)
@@ -36,3 +42,29 @@ def test_listener_can_delete():
     listener.start(1)
 
     assert os.path.isfile(srcPath + "/in/testid2_1_encoded.mp4") == False
+
+def test_listener_can_handle_known_error():
+    globals()["knownErrorDetected"] = False
+
+    listener = DirectoryListener(srcPath + "/in", ["README.md"], processFilesWithKnownError, knownErrorCallback)
+    open(srcPath + "/in/testid2_1_encoded.mp4", "+a").close()
+
+    listener.start(1)
+
+    assert globals()["knownErrorDetected"] == True
+    assert os.path.isfile(srcPath + "/in/testid2_1_encoded.mp4") == False
+    assert os.path.isfile(srcPath + "/in/poison/testid2_1_encoded.mp4") == True
+    os.remove(srcPath + "/in/poison/testid2_1_encoded.mp4")
+
+def test_listener_can_handle_unknown_error():
+    globals()["knownErrorDetected"] = False
+
+    listener = DirectoryListener(srcPath + "/in", ["README.md"], processFilesWithUnexpectedError, knownErrorCallback)
+    open(srcPath + "/in/testid2_1_encoded.mp4", "+a").close()
+
+    listener.start(1)
+
+    assert globals()["knownErrorDetected"] == False
+    assert os.path.isfile(srcPath + "/in/testid2_1_encoded.mp4") == False
+    assert os.path.isfile(srcPath + "/in/poison/testid2_1_encoded.mp4") == True
+    os.remove(srcPath + "/in/poison/testid2_1_encoded.mp4")
