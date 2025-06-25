3e711893486f48190935c99dc88ece83dfff5617
ru1ve <113336003+ru1ve@users.noreply.github.com>
2025-06-25 20:19:41 +0100

    Fixed an issue with Unit tests #2

--- src/_tests/test_videoMetrics.py
+++ src/_tests/test_videoMetrics.py
@@ -4,11 +4,12 @@ import sys
 
 REF_PATH = "../../test_videos/sample_source.mp4"
 DIS_PATH = "../../test_videos/sample_degraded.mp4"
+DIF_PATH = "../../test_videos/sample_different.mp4"
 
-@unittest.skipUnless(
-    sys.platform.startswith("win"),
-    "Custom FFmpeg build currently only runs on windows"
-)
+# @unittest.skipUnless(
+#     sys.platform.startswith("win"),
+#     "Custom FFmpeg build currently only runs on windows"
+# )
 class MyTestCase(unittest.TestCase):
 
     def test_sameVideo(self):
@@ -21,7 +22,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(float(ssim), 1.0)
         self.assertEqual(psnr, "inf")
 
-    def test_DifferentVideo(self):
+    def test_DegradedVideo(self):
         test_harness = VMAFEvaluator(reference_path=REF_PATH, distorted_path=DIS_PATH)
 
         vmaf, ssim, psnr = test_harness.evaluate()
@@ -29,6 +30,15 @@ class MyTestCase(unittest.TestCase):
         self.assertGreater(float(vmaf), 90.0) #96 is a high VMAF score -> identical videos
         self.assertEqual(float(ssim), 0.983802)
         self.assertEqual(float(psnr), 42.773112)
+
+    def test_DifferentVideo(self):
+        test_harness = VMAFEvaluator(reference_path=REF_PATH, distorted_path=DIF_PATH)
+
+        vmaf, ssim, psnr = test_harness.evaluate()
+
+        self.assertEqual(float(vmaf), 0.0) # Should be 0 completely different videos
+        self.assertLess(float(ssim), 0.5) # should be low value
+        self.assertLess(float(psnr), 20) # Fairly low value
 
 
 if __name__ == '__main__':
