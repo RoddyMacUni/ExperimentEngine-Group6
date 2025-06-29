3d27467d626a03c58422d908a668b3dca5bafdf1
RoddyMacUni <roderick.macrae.2022@uni.strath.ac.uk>
2025-06-29 13:53:47 +0100

    Add validation for various potential issues as well as reporting for any future log file parsing

--- src/network_emulation/virtual-network.sh
+++ src/network_emulation/virtual-network.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 ## PARAMS
 # $1 - The sequence number of the video
 # $2 - The network delay in ms
@@ -8,26 +7,53 @@
 # $4 - The source video filepath
 # $5 - The distorted video output filepath
 
-# Introduce the network conditions - substituted for echo commands for testing purposes
-echo "Running experiment " $1 > result.txt
-echo "Delay: " $2 >> result.txt
-echo "Packet Loss: " $3 >> result.txt
 
-# Ensure the netem kernel module is installed
+# Output the params that have been provided
+echo "[EE_INFO] Sequence_number" "$1"
+echo "[EE_INFO] Delay: " "$2"
+echo "[EE_INFO] Packet Loss: " "$3"
+echo "[EE_INFO] Source file: " "$4"
+echo "[EE_INFO] Distorted file path: " "$5"
+
+# Load the required kernel module and validate it has installed correctly
 modprobe sch_netem
-lsmod | grep sch_netem
+if [[ -z $(lsmod | grep sch_netem) ]]; then
+  echo "[OS_ERROR] module sch_netem unable to be installed"
+  exit 1
+fi
+
+# Apply the requested disruption to the loopback adapter and validate it has been applied
+tc qdisc add dev lo root netem delay "$2"ms loss "$3"%
+if [[ -z $(tc qdisc show dev lo | grep 'qdisc netem') ]]; then
+  echo "[OS_ERROR] network conditions unable to be applied"
+  exit 1
+fi
+
+# Delete the previously exported video if it already exists
+if [[ -a $5 ]]; then
+  rm "$5"
+fi
 
 # Generate sdp file for this experiment
-ffmpeg -sdp_file /tmp/experiment.sdp
+ffmpeg -re -i "$4" -c:v copy -an -f rtp -sdp_file /tmp/experiment.sdp "rtp://127.0.0.1:1234" &
 
-# Apply the requested disruption to the loopback adapter
-tc qdisc add dev lo root netem delay $2 loss $3
+sdp_timeout=5
 
-# Start the stream through the network in the background
-ffmpeg -re -i $4 -c:v copy -an -f rtp -sdp_file /tmp/experiment.sdp "rtp://127.0.0.1:1234" &
+# Wait until the SDP file has been created then start the streaming
+# If the sdp file is not created within 5 seconds the script will fail and terminate the test
+while [[ -a /tmp/experiment.sdp ]]
+do
+  if [[ $sdp_sleeptime -ge $sdp_timeout ]]; then
+    echo "[STREAMING_ERROR] sdp file not created before timeout"
+    exit 1
+  fi
+
+  ((sdp_sleeptime++))
+  sleep .1
+done
 
 # Start the recipient stream and save received video to file
-ffmpeg -protocol_whitelist file,rtp,udp -i /tmp/experiment.sdp -strict 2 $5
+ffmpeg -protocol_whitelist file,rtp,udp -i /tmp/experiment.sdp -strict 2 "$5"
 
 # Stop the stream transmitting once the video has been received
 kill %1
