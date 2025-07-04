6cef8ed210999208fe895eda4b91c09e64fd18e6
JJInnes <joshinnes05@gmail.com>
2025-07-04 12:28:19 +0100

    added token refreshing

--- src/api/TokenManager.py
+++ src/api/TokenManager.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from dacite import from_dict
 import requests
 import json
+import datetime
 
 @dataclass
 class tokenResponse:
@@ -11,20 +12,25 @@ class tokenResponse:
 
 class TokenManager(BaseApi):
     token: str | None
+    lastFetched: str | None
+    refreshSeconds: int
 
-    def __init__(self, baseUrl):
+    def __init__(self, baseUrl, refreshSeconds: int = 300):
         super().__init__(baseUrl)
         self.token = None
+        self.lastFetched = None
+        self.refreshSeconds = refreshSeconds
 
     def fetchNewToken(self):
         appsettings: AppSettings = GetAppSettings()
         response = requests.post(self.baseUrl + "/auth/login", json={'username': appsettings.Username,'password': appsettings.Password})
         response.raise_for_status()
         responseObject: tokenResponse = from_dict(data_class=tokenResponse, data=json.loads(response.text))
+        self.lastFetched = datetime.datetime.now()
         return responseObject.token
 
     def getToken(self):
-        if(self.token is None):
+        if(self.token is None or datetime.datetime.now > self.lastFetched + datetime.timedelta(0, self.refreshSeconds)):
             self.token = self.fetchNewToken()
         return self.token
     
