a8fde99b75fd657aa78cb097573df85788e4614f
JJInnes <joshinnes05@gmail.com>
2025-07-04 12:32:29 +0100

    added unit test

--- src/_tests/test_tokenManager.py
+++ src/_tests/test_tokenManager.py
@@ -1,5 +1,6 @@
 import requests_mock
 from api.TokenManager import TokenManager
+import time
 
 tm: TokenManager = TokenManager("http://localhost:2000/fake")
 m = requests_mock.Mocker()
@@ -20,3 +21,17 @@ def test_will_fetch_new_token_if_none():
         tm.token = None
 
         assert tm.getToken() == "abc"
+
+def test_will_refresh_token():
+    with requests_mock.Mocker() as m:
+        tm: TokenManager = TokenManager("http://localhost:2000/fake", 1)
+        m.post('http://localhost:2000/fake/auth/login', text='{"token": "abc"}')
+
+        tm.token = None
+
+        assert tm.getToken() == "abc"
+
+        m.post('http://localhost:2000/fake/auth/login', text='{"token": "abcd"}')
+        assert tm.getToken() == "abc"
+        time.sleep(2)
+        assert tm.getToken() == "abcd"
